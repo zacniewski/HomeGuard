d000e570d7d985ad6003c9917ccdb855ee8f60ad
Artur Zacniewski <a.zacniewski@gmail.com>
2020-09-15 20:08:37 +0200

    Static and media settings of project

--- homeguard/settings.py
+++ homeguard/settings.py
@@ -125,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
+# ścieżka do zmiany na innym komputerze !!!
+# STATIC_ROOT = "/home/pi/HomeGuard/static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "/home/pi/HomeGuard/staticfiles"
+]

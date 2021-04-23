b1a571027abbfc1bd7001a6be94b1374a96a5235
Artur Zacniewski <a.zacniewski@gmail.com>
2021-04-23 12:41:09 +0200

    Wired network data captured

--- frontend/views.py
+++ frontend/views.py
@@ -12,10 +12,14 @@ class IndexView(TemplateView):
 def network(request):
     # 'wlan0' for RPi and 'wlp5s0' for my laptop
     result_of_iwconfig = subprocess.run(['iwconfig', 'wlan0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    result_of_ifconfig = subprocess.run(['ifconfig', 'wlp5s0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result_of_ifconfig = subprocess.run(['ifconfig', 'eth0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     list_with_iwconfig_data = result_of_iwconfig.split('\n')
+    list_with_ifconfig_data = result_of_ifconfig.split('\n')
+
     iwconfig_dict = {}
+    ifconfig_dict = {}
+
     for item in list_with_iwconfig_data:
         if 'SSID' in item:
             iwconfig_dict['name'] = item[item.find('\"')+1: item.rfind('\"')]
@@ -25,9 +29,11 @@ def network(request):
             iwconfig_dict['BitRate'] = item[item.find('Rate')+5: item.rfind('b/s')+3]
         if 'Quality' in item:
             iwconfig_dict['LinkQuality'] = item[item.find('Quality')+8: item.find('=')+6]
-        print(item.strip())
-    print(iwconfig_dict)
-    return render(request, 'network.html', {'iwconfig_dict': iwconfig_dict})
+
+    for item in list_with_ifconfig_data:
+        if 'broadcast' in item:
+            ifconfig_dict['ip'] = item[item.find('inet')+4: item.rfind('netmask')-1]
+        return render(request, 'network.html', {'iwconfig_dict': iwconfig_dict}, {'ifconfig_dict': ifconfig_dict})
 
 
 def updates(request):

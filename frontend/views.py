5bf17521711c0da17cf57a4aaf426e79e0a786a6
Artur Zacniewski <a.zacniewski@gmail.com>
2021-04-23 12:58:31 +0200

    Wired network data captured in dict

--- frontend/views.py
+++ frontend/views.py
@@ -17,23 +17,22 @@ def network(request):
     list_with_iwconfig_data = result_of_iwconfig.split('\n')
     list_with_ifconfig_data = result_of_ifconfig.split('\n')
 
-    iwconfig_dict = {}
-    ifconfig_dict = {}
+    network_dict = {}
 
     for item in list_with_iwconfig_data:
         if 'SSID' in item:
-            iwconfig_dict['name'] = item[item.find('\"')+1: item.rfind('\"')]
+            network_dict['name'] = item[item.find('\"')+1: item.rfind('\"')]
         if 'Frequency' in item:
-            iwconfig_dict['Frequency'] = item[item.find('Frequency')+10: item.rfind('Hz')+2]
+            network_dict['Frequency'] = item[item.find('Frequency')+10: item.rfind('Hz')+2]
         if 'Rate' in item:
-            iwconfig_dict['BitRate'] = item[item.find('Rate')+5: item.rfind('b/s')+3]
+            network_dict['BitRate'] = item[item.find('Rate')+5: item.rfind('b/s')+3]
         if 'Quality' in item:
-            iwconfig_dict['LinkQuality'] = item[item.find('Quality')+8: item.find('=')+6]
+            network_dict['LinkQuality'] = item[item.find('Quality')+8: item.find('=')+6]
 
     for item in list_with_ifconfig_data:
         if 'broadcast' in item:
-            ifconfig_dict['ip'] = item[item.find('inet')+4: item.rfind('netmask')-1]
-        return render(request, 'network.html', {'iwconfig_dict': iwconfig_dict, 'ifconfig_dict': ifconfig_dict})
+            network_dict['wired_ip'] = item[item.find('inet')+4: item.rfind('netmask')-1]
+        return render(request, 'network.html', {'network_dict': network_dict})
 
 
 def updates(request):

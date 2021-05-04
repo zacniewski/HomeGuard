5d951b7375228151f2e368e3da5b35e527f17896
Artur Zacniewski <a.zacniewski@gmail.com>
2021-05-04 21:42:08 +0200

    Streaming in thread with VideoStream

--- homeguard/settings.py
+++ homeguard/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend.apps.FrontendConfig',
+    'vision.apps.VisionConfig',
 ]
 
 MIDDLEWARE = [
--- vision/singlemotiondetector.py
+++ vision/singlemotiondetector.py
@@ -17,7 +17,7 @@ class SingleMotionDetector:
             return
         # update the background model by accumulating the weighted
         # average
-        cv2.accumulateWeighted(image, self.bg, self.accumWeight)
+        cv2.accumulateWeighted(image, self.bg, self.accum_weight)
 
     def detect(self, image, tVal=25):
         # compute the absolute difference between the background model
@@ -28,4 +28,26 @@ class SingleMotionDetector:
         # blobs
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in the thresholded image and initialize the
+        # minimum and maximum bounding box regions for motion
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        (minX, minY) = (np.inf, np.inf)
+        (maxX, maxY) = (-np.inf, -np.inf)
+
+        # if no contours were found, return None
+        if len(cnts) == 0:
+            return None
+        # otherwise, loop over the contours
+        for c in cnts:
+            # compute the bounding box of the contour and use it to
+            # update the minimum and maximum bounding box regions
+            (x, y, w, h) = cv2.boundingRect(c)
+            (minX, minY) = (min(minX, x), min(minY, y))
+            (maxX, maxY) = (max(maxX, x + w), max(maxY, y + h))
+        # otherwise, return a tuple of the thresholded image along
+        # with bounding box
+        return (thresh, (minX, minY, maxX, maxY))
 
